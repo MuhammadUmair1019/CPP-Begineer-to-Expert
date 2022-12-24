b3664fb3370e9aca71e4396bd16c4479e629d131
Muhammad umair <muhammad.umair1019@gmail.com>
2022-12-24 18:58:16 +0500

    pascals triangle pattren

--- easy-problems/CPP015.cpp
+++ easy-problems/CPP015.cpp
@@ -368,14 +368,13 @@ int main()
   }
 }
 
-// reverse full pyramid triangle 
-// * * * * * * 
-//  * * * * * 
-//   * * * * 
-//    * * * 
-//     * * 
-//      * 
-
+// reverse full pyramid triangle
+// * * * * * *
+//  * * * * *
+//   * * * *
+//    * * *
+//     * *
+//      *
 
 #include <iostream>
 using namespace std;
@@ -397,6 +396,35 @@ int main()
     for (int j = 1; j <= i; j++)
     {
       cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+// Pascale triangel
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int rows;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  for (int i = 1; i <= rows; i++)
+  {
+    int coef = 1;
+    for (int k = rows - i; k > 0; k--)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << coef << " ";
+      coef = coef * (i - j) / j;      
     }
     cout << endl;
   }
