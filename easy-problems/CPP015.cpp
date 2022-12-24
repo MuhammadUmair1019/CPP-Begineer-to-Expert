d1a59f1eac5e694b87897234537da4ad23d2e3e6
Muhammad umair <muhammad.umair1019@gmail.com>
2022-12-24 18:25:11 +0500

    full trianlge pyramid pattren

--- easy-problems/CPP015.cpp
+++ easy-problems/CPP015.cpp
@@ -143,18 +143,21 @@ int main()
 #include <iostream>
 using namespace std;
 
-int main () {
+int main()
+{
 
   int rows;
 
   cout << "Enter number of rows: ";
   cin >> rows;
 
-  for(int i=1; i <= rows; i++) {
-    for (int j=1; j <= i; j++) {
-      cout << j<<" ";
+  for (int i = 1; i <= rows; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << j << " ";
     }
-    cout <<"\n";
+    cout << "\n";
   }
 }
 
@@ -278,6 +281,123 @@ int main()
     {
       cout << "*";
     }
-    cout<<"\n";
+    cout << "\n";
+  }
+}
+
+// ----------------------------------------------------
+// Simple trangle /half pyramid (incremental number)
+// 1
+// 22
+// 333
+// 4444
+// 55555
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int rows;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  for (int i = 1; i <= rows; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << i;
+    }
+    cout << "\n";
+  }
+}
+
+// 11111
+// 2222
+// 333
+// 44
+// 5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int rows;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  for (int i = rows; i >= 1; i--)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << rows - i + 1;
+    }
+
+    cout << "\n";
+  }
+}
+
+// Full pyramid triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int rows;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  for (int i = 1; i <= rows; i++)
+  {
+    for (int k = rows - i; k > 0; k--)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+// reverse full pyramid triangle 
+// * * * * * * 
+//  * * * * * 
+//   * * * * 
+//    * * * 
+//     * * 
+//      * 
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int rows;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  for (int i = rows; i >= 1; i--)
+  {
+    for (int k = rows - i; k > 0; k--)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
   }
 }
